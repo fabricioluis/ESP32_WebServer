868f86a7cba5c23a3ad21998ae61cb71102c0f74
Fabricio Luis <ce3po.robo@gmail.com>
2024-12-25 00:22:53 -0300

    Implementa acesso WiFi via WiFiServer.

--- src/main.cpp
+++ src/main.cpp
@@ -7,58 +7,117 @@ int ct = 0;
 uint32_t memRAM_anterior = ESP.getFreeHeap();  // system_get_free_heap_size();
 uint32_t memRAM;
 
-WebServer server(80);
+WiFiServer server(80);
 
-void contador();
 void handle();
+
+/*
+void contador();
 void teste();
+*/
+
 void printMemoria(const char *pMsg = NULL);
 
 void setup()
 {
   Serial.begin(115200);
+  delay(1000);
+
+  // WiFi.mode(WIFI_STA);
   WiFi.begin(NET_SSID, NET_PASSWORD);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+
   Serial.printf("\nWiFi conectado\n");
   Serial.printf("IP: %s\n", WiFi.localIP().toString());
-
-  server.on("/", handle);
-  server.on("/ct", contador);
-  server.on("/teste", teste);
 
   server.begin();
 
   Serial.printf("Server HTTP Ok.\n");
+  delay(1000);
 }
 
 void loop()
 {
-  server.handleClient();
+  handle();
 }
 
 void handle()
 {
-  String response = "<html>\n<center><h3>ESP32 - Wifi - WebServer</h3></center><br>\n";
-  response += "<a href='/ct'>Contador</a><br>\n";
-  response += "<a href='/teste'>Teste</a><br>\n";
-  response += "<a href='/'>Inicio</a><br>\n";
-  response += "</html>";
-  server.send(200, "text/html", response);
+  WiFiClient client = server.available();
 
-  printMemoria();
+  if (client) {
+    Serial.printf("III: %s\n\n", client.remoteIP().toString());
+
+    String url = "";
+    String currentLine = "";
+    char c;
+
+    while (client.connected()) {
+
+      c = client.read();
+      url += c;
+      Serial.write(c);
+
+      if (url.indexOf("HTTP/1.1") >= 0) {
+        Serial.printf("\nurl:%s\n", url.c_str());
+        url = "";
+      }
+
+      if (c == '\n') {
+        if (currentLine.length() == 0) {
+          String response = "<html>\n<center><h3>ESP32 - Wifi - WebServer</h3></center><br>\n";
+          response += "<a href='/ct'>Contador</a><br>\n";
+          response += "<a href='/teste'>Teste</a><br>\n";
+          response += "<a href='/'>Inicio</a><br>\n";
+          response += "</html>";
+          client.println(response);
+          break;
+        }
+        else {
+          currentLine = "";
+        }  // if (currentLine.length() == 0) {
+      }
+      else if (c != '\r') {
+        currentLine += c;
+      }  // if (c == '\n') {
+    }
+
+    client.stop();
+    Serial.println("FFF - Cliente desconectado.");
+
+    printMemoria();
+  }
+}  // if (client) {
+
+void printMemoria(const char *pMsg)
+{
+  // https://github.com/esp8266/esp8266-wiki/wiki/Memory-Map
+
+  // Verifica se a memoria RAM diminui.
+  memRAM = ESP.getFreeHeap();  // system_get_free_heap_size();
+  if (pMsg)
+    Serial.printf(
+        "\nMem.: %d-%d=%d %s\n", memRAM_anterior, memRAM, (memRAM_anterior - memRAM), pMsg);
+  else
+    Serial.printf("\nMem.: %d-%d=%d\n", memRAM_anterior, memRAM, (memRAM_anterior - memRAM));
+
+  // system_print_meminfo();
+  // system_show_malloc();
+  memRAM_anterior = memRAM;
 }
 
+/*
 void contador()
 {
   String response = "Contador: ";
   response += ct++;
   server.send(200, "text/plain", response);
 
-  printMemoria();
+  // printMemoria();
 }
 
 // Função para lidar com a rota "/teste"
@@ -79,22 +138,7 @@ void teste()
   server.send(200, "text/html", response);
   //  server.send(200, "text/plain", response);
 
-  printMemoria();
+  // printMemoria();
 }
 
-void printMemoria(const char *pMsg)
-{
-  // https://github.com/esp8266/esp8266-wiki/wiki/Memory-Map
-
-  // Verifica se a memoria RAM diminui.
-  memRAM = ESP.getFreeHeap();  // system_get_free_heap_size();
-  if (pMsg)
-    Serial.printf(
-        "\nMem.: %d-%d=%d %s\n", memRAM_anterior, memRAM, (memRAM_anterior - memRAM), pMsg);
-  else
-    Serial.printf("\nMem.: %d-%d=%d\n", memRAM_anterior, memRAM, (memRAM_anterior - memRAM));
-
-  // system_print_meminfo();
-  // system_show_malloc();
-  memRAM_anterior = memRAM;
-}
+*/
